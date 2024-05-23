d320f1e9c218d1c7d8894065e0aa3b60b3c682f7
byiringiroaimefils <aimefils173@gmail.com>
2024-05-23 21:48:50 +0200

    New updates

--- FrontEnd/src/Components/Froms/FormProverb.tsx
+++ FrontEnd/src/Components/Froms/FormProverb.tsx
@@ -30,7 +30,7 @@ function Component() {
             })
             .catch((error) => {
                 console.log(error)
-                toast.error("This didn't work")
+                toast.error("This didn't  work!!!!")
             })
     }
 
