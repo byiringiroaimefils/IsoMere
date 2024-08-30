0dac4477c83ae8d61279e844b6aa3cf03984c5b3
byiringiroaimefils <aimefils173@gmail.com>
2024-08-30 20:53:07 +0200

    Server

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -255,10 +255,80 @@ App.put("/EditProverb/:id", (req, resp) => {
 
 
 
+// // // -----SERVER FOR PROVERBS-----
+// // const proverbs = Mongoose.model("Proverbs", ProverbSchema, "Proverbs");
+// // App.post("/proverb", (req, resp) => {
+
+// //   const newProverb = {
+// //     TitleofProverb: req.body.Tofproverb,
+// //     Proverb: req.body.Proverb
+// //   };
+
+
+// //   const Proverb = proverbs.create(newProverb)
+// //     .then((data) => {
+// //       resp.json(data);
+// //       console.log(data.data)
+// //     })
+// //     .catch((err) => {
+// //       console.log("Error", err);
+// //     });
+// // });
+
+
+// App.get("/proverb/:id", (req, resp) => {
+//   const { id } = req.params;
+//   const selectproverb = proverbs.findById(id)
+//     .then((data) => {
+//       resp.json(data);
+//     })
+//     .catch((err) => {
+//       console.log("Error", err);
+//     });
+// });
+
+// App.get("/proverbs", (req, resp) => {
+//   const selectProverb = proverbs.find()
+//     .then((data) => {
+//       resp.json(data);
+//     })
+//     .catch((err) => {
+//       console.log("Error", err);
+//     });
+// });
+
+// App.delete("/deleteProverb/:id", (req, resp) => {
+//   const { id } = req.params;
+//   const book = proverbs.findByIdAndDelete(id)
+//     .then((data) => {
+//       resp.json("Deleted");
+//     })
+//     .catch((err) => {
+//       console.log("Error", err);
+//     });
+// });
+
+// App.put("/EditProverb/:id", (req, resp) => {
+
+//   const newProverb = {
+//     TitleofProverb: req.body.Tofproverb,
+//     Proverb: req.body.Proverb
+//   };
+//   const { id } = req.params;
+//   const proverb = proverb.findByIdAndUpdate(id, newProverb)
+//     .then((data) => {
+//       resp.json("Updated");
+//     })
+//     .catch((err) => {
+//       console.log("Error", err);
+//     });
+// });
 
 
 
-//PORT
+
+
+// Listener of Port
 App.listen(Port, () => {
   console.log(`This app is running on ${Port}`);
 });
